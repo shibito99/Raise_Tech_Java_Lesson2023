c64c5695ca861cfbf6f6fcfdd7bcb9ff98f009f2
kenichi-furuya <96564831+shibito99@users.noreply.github.com>
2023-01-09 19:56:07 +0900

    Create Kadai_1.java

---
+++ src/Kadai_1.java
@@ -0,0 +1,15 @@
+package com.example.Java_Lesson;
+
+public class kadai_1 {
+	public static void main(String[] args) {
+		System.out.print("Hello_worldの一回表示\n");
+		System.out.print("Hello_world.\n");
+		System.out.print("================\n");
+
+		for (int i = 0; i < 10; i++) {
+			//"Hello_Worldの繰り返し"
+			System.out.println("Hello_World");
+		}
+	}
+}
+
